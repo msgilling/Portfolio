d32dd3c4e8716dea1e13f69a50e3aa864a9d29c7
Mariah Gilling <mxrlling@gmail.com>
2021-12-29 21:07:15 +0000

    about me page complete

--- src/components/About.js
+++ src/components/About.js
@@ -10,8 +10,10 @@ const About = () => {
         <div className="about-container">
           <img src={LOGO} alt="logo" className="logo"/>
         <div className="about-para">
-          <p>Hi, my name is Mariah!</p>
-          <p>I recently pursued a career change into Software Engineering after working as a Visual Merchandiser.</p>
+          <p>Hi, my name is <strong>Mariah!</strong></p>
+          <p>My previous role as a Visual Merchandiser allowed me to be creative and problem solve, two aspects of the job I really became really passionate about. This along with doing illustration outside of the workplace encouraged me to look into coding and found a huge love for it. </p>
+          <p>This drove me to go and study coding and software engineering at General Assembly, enjoying the ability to learn as much as I can with like minded people.</p>
+          <p>I am now looking to start my career as a Developer in a collaborative team where I can continue to grow my skills daily as a coder and collaborate with like minded individuals and share my creativity.</p>
         </div>
         </div>
       </div>
