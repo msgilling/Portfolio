47afb10184489f7be805e4cd7377cc77ae82d5b8
Mariah Gilling <mxrlling@gmail.com>
2022-01-05 21:21:36 +0000

    experience component added

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import About from './components/About'
 // import NavBar from './components/NavBar'
 import Portfolio from './components/Portfolio'
 import Contact from './components/Contact';
+import Experience from './components/Experience';
 
 function App() {
   return (
-   <BrowserRouter>
-    {/* <NavBar/> */}
-   <Switch>
-     <Route exact path="/" component={Home}/>
-     <Route exact path="/about" component={About}/>
-    
-     <Route exact path="/projects" component={Portfolio}/>
-     <Route exact path="/contact" component={Contact}/>
-   </Switch>
+    <BrowserRouter>
+      {/* <NavBar/> */}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Portfolio} />
+        <Route exact path="/experience" component={Experience} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
 
-   </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
-import React from 'react' 
+import React from 'react'
 import NavBar from './NavBar'
 import { FiMail } from 'react-icons/fi'
-import { FaLinkedin,FaGithub } from 'react-icons/fa'
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
 import PIC from '../assets/profilepic.jpg'
 import Fade from 'react-reveal'
 
@@ -9,39 +9,39 @@ const Contact = () => {
 
   return (
     <>
-    <NavBar/>
-    <div className="contact-section">
-      <Fade top>
-      <h1 className="contact-title">&bull; Contact &bull;</h1>
-      </Fade>
-      <div className="contact-container">
-      <Fade left>
-        <img src={PIC} alt="logo" className="profilepic"/>
-      </Fade>
-      
-      <Fade right>
-        <div className="contact-options">
-          <FiMail size={35} color="white" className="icon-contact" />
-          <a href="mailto:mxrlling@gmail.com" className="contact-links">Email</a>
+      <NavBar />
+      <div className="contact-section">
+        <Fade top>
+          <h1 className="contact-title">&bull; Contact &bull;</h1>
+        </Fade>
+        <div className="contact-container">
+          <Fade left>
+            <img src={PIC} alt="logo" className="profilepic" />
+          </Fade>
+
+          <Fade right>
+            <div className="contact-options">
+              <FiMail size={35} color="white" className="icon-contact" />
+              <a href="mailto:mxrlling@gmail.com" className="contact-links">Email</a>
+            </div>
+          </Fade>
+          <Fade left>
+            <div className="contact-options">
+              <FaLinkedin size={35} color="white" className="icon-contact" />
+              <a href="https://www.linkedin.com/in/mariah-gilling/" className="contact-links">LinkedIn</a>
+            </div>
+          </Fade>
+          <Fade right>
+            <div className="contact-options">
+              <FaGithub size={35} color="white" className="icon-contact" />
+              <a href="https://github.com/msgilling" className="contact-links">GitHub</a>
+            </div>
+          </Fade>
+
         </div>
-      </Fade>
-      <Fade left>
-        <div className="contact-options">
-          <FaLinkedin size={35} color="white" className="icon-contact" />
-          <a href="https://www.linkedin.com/in/mariah-gilling/" className="contact-links">LinkedIn</a>
-        </div>
-      </Fade>
-      <Fade right>
-        <div className="contact-options">
-          <FaGithub size={35} color="white" className="icon-contact" />
-          <a href="https://github.com/msgilling" className="contact-links">GitHub</a>
-        </div>
-      </Fade>
-      
+
       </div>
 
-    </div>
-  
     </>
   )
 }
